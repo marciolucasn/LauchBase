0665a99c15149f9bee97e2f41dcda2e9346ddf4c
Márcio Lucas <n.marciolucas@gmail.com>
2020-04-07 14:26:46 -0300

    Desafios 1-2

---
+++ Desafios/desafio1-2B.js
@@ -0,0 +1,20 @@
+const programador = {
+    nome: "Márcio",
+    idade: 20,
+    tecnologias: [
+        {
+            nome: "C++",
+            especialidade: "Desktop"
+        },
+        {
+            nome: "Python",
+            especialidade: "Data Science"
+        },
+        {
+            nome: "JavaScript",
+            especialidade: "Web/Mobile"
+        }
+    ]
+}
+
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}.`)
